f24c936970411739fd1729ffabc3e0a21fe6a118
Gabriel Vilela <gm.vilela@uol.com.br>
2024-10-15 23:43:14 -0300

    Atualização: DbContext ; Entities ; Mappings ; Migration ; Repositores ; DTOs

--- BlazorShop.Api/Program.cs
+++ BlazorShop.Api/Program.cs
@@ -1,3 +1,7 @@
+using BlazorShop.Api.Context;
+using BlazorShop.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 
 var app = builder.Build();
 
