1a122cad5d48ca88bbd77335972f47bb411228a9
Gabriel Vilela <gm.vilela@uol.com.br>
2024-10-24 23:05:08 -0300

    Criacao componetes ExibirErro-ExibirSpinner-Paginas Catalogo-ExibirProdutos-ProdutosDetalhes e Services

--- BlazorShop.Web/Services/ProdutoService.cs
+++ BlazorShop.Web/Services/ProdutoService.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorShop.Models.DTOs;
+using System.Net;
 using System.Net.Http.Json;
 
 namespace BlazorShop.Web.Services;
@@ -26,6 +27,34 @@ public class ProdutoService : IProdutoService
         catch (Exception)
         {
             _logger.LogError("Erro ao acessar produtos : api/produtos");
+            throw;
+        }
+    }
+
+    public async Task<ProdutoDto> GetItem(int id)
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync($"api/produtos/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == HttpStatusCode.NoContent)
+                {
+                    return default(ProdutoDto);
+                }
+                return await response.Content.ReadFromJsonAsync<ProdutoDto>();
+            }
+            else
+            {
+                var message = await response.Content.ReadAsStringAsync();
+                _logger.LogError($"Erro a obter produto pelo id= {id} - {message}");
+                throw new Exception($"Status Code : {response.StatusCode} - {message}");
+            }
+        }
+        catch (Exception)
+        {
+            _logger.LogError($"Erro a obter produto pelo id={id}");
             throw;
         }
     }
